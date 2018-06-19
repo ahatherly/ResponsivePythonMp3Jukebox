715cdfb265e08c545343e180c77a05218e627593
ahatherly <adam@hatherly.com>
2018-06-19 22:07:40 +0100

    Fixes for cover art download

--- coverart.py
+++ coverart.py
@@ -1,21 +1,26 @@
 import sacad
+import asyncio
+import logging
+from sacad.cover import SUPPORTED_IMG_FORMATS
 
 class CoverArt:
 
     def getCoverArt(self, artist, album):
         # search and download
-
-        """
-        sacad.search_and_download(album,
+        async_loop = asyncio.get_event_loop()
+        logging.getLogger().setLevel(logging.DEBUG)
+        coroutine = sacad.search_and_download(album,
                                   artist,
-                                  "JPEG",
+                                  SUPPORTED_IMG_FORMATS["jpg"],
                                   600,
-                                  "/tmp/cover.jpg",
+                                  "cover.jpg",
                                   size_tolerance_prct=25,
-                                  amazon_tlds="",
+                                  amazon_tlds=["co.uk"],
                                   no_lq_sources=False,
-                                  async_loop="")
-        """
-        args = []
-        print(sacad.cl_main(args))
+                                  async_loop=async_loop)
+        future = asyncio.ensure_future(coroutine, loop=async_loop)
+        async_loop.run_until_complete(future)
 
+if __name__ == "__main__":
+	art = CoverArt()
+	art.getCoverArt("Metallica","Master of Puppets")
